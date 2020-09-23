c238e80a7113d64c695094756152b33d1f261418
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-09-23 12:56:52 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,7 +4,7 @@
 # Two steps equations are in the format ax + b = c
 # You will ask the user to enter in all 3 variables: a, b and c
 # You will need to display the solution for the equation
-
+# hi
 # inputs
 # a, b, c
 #
